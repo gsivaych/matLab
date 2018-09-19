9106e41b67ba52087d64bd0d2290620c063c98ad
gsivaych <gauravsinghsivaych@gmail.com>
2018-09-19 16:42:23 +0530

    fixed point iterations

--- numericalAnalysis/fixedPointMethod.m
+++ numericalAnalysis/fixedPointMethod.m
@@ -10,6 +10,7 @@ x0 = input('Initial guess x0 : ');
 tolerance = input('Tolerance required : ');
 maxIterations = input('Max. iterations : ');
 
+gotRoot = 0;
 iterationNum = 1 ;
 while(iterationNum < maxIterations)
     x1 = g(x0); % point on curve
@@ -17,6 +18,7 @@ while(iterationNum < maxIterations)
     if abs(x1-x0) < tolerance
         fprintf('Fixed point of g(x) i.e. root of f(x) precise to %d is : %d\n',tolerance,x1);
         fprintf('Iterations required : %d\n',iterationNum);
+        gotRoot = 1;
         break
     else
         x0 = x1;    % point on line y = x
@@ -24,11 +26,15 @@ while(iterationNum < maxIterations)
     end
 end
 
-% Plot
-figure('Name','Fixed Point','NumberTitle','off','Color','w')
-x = x0-10:0.01:x0+10;
-plot(x,g(x),'b'), grid on, grid minor,
-title(['Fixed point iteration of g(x) = ',equation])
-xlabel('x'), ylabel('g(x)')
-hold on
-plot(x1,g(x1),'ro')
+if gotRoot
+    % Plot
+    figure('Name','Fixed Point','NumberTitle','off','Color','w')
+    x = x0-10:0.01:x0+10;
+    plot(x,g(x),'b'), grid on, grid minor,
+    title(['Fixed point iteration of g(x) = ',equation])
+    xlabel('x'), ylabel('g(x)')
+    hold on
+    plot(x1,g(x1),'ro')
+else
+    disp('Maximum iterations reached : Could not find root/fixed point.')
+end
